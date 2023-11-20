37d46e4853e774e064df6f01d8a679af67f254be
anushkapoonia07 <anushkapoonia53@gmail.com>
2023-11-20 08:50:29 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -25,6 +25,16 @@ const imageData = ()=>{
 
 function App() {
   // code here
+  const ImgData=imageData();
+  return(
+    <div>
+      <div><h1>Kalvium gallary</h1></div>
+      <div>{ImgData.map((item) => {
+      return <img key={item.id} src={item.img} alt="" />
+      })}
+      </div>
+   </div>
+  )
 }
 
 export default App;
--- src/AppClass.jsx
+++ src/AppClass.jsx
@@ -27,4 +27,16 @@ export default class AppClass extends Component{
   }
 
   // code here
+  render(){
+    const ImgData= this.imageData();
+  return(
+    <div>
+      <div><h1>Kalvium gallary</h1></div>
+      <div>{ImgData.map((item) => {
+      return <img key={item.id} src={item.img} alt="" />
+      })}
+      </div>
+   </div>
+  )
+  }
 }
